d1ce31c3d05d5a4e8b8c1b962a206b0fd533e03f
shivam seamar <130454074+ShivamSeamar@users.noreply.github.com>
2023-09-16 11:55:56 +0530

    Update Operaters.java

--- Basic of java/Operaters.java
+++ Basic of java/Operaters.java
@@ -2,11 +2,11 @@
 
 public class Operaters {
     public static void main(String[] args){
-       /* int A = 10;
+        int A = 10;
         int B = 20;
         System.out.println("add = " + (A+B));
         System.out.println("sub  = " + (A-B));
-        System.out.println("modulo  = " + (A%B));*/
+        System.out.println("modulo  = " + (A%B));
 
 
         /*int a = 10;  //pre increment
